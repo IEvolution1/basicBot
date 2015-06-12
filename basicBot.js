f36886718b375ab6f305a16a933e84e6236f2431
IEvolution1 <xmark95@me.com>
2015-06-12 23:22:43 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2588,7 +2588,6 @@
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var msg = chat.message;
                         if (msg.length === cmd.length) return API.sendChat(subChat(basicBot.chat.nouserspecified, {name: chat.un}));
